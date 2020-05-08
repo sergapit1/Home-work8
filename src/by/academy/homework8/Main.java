09cf988a8ebe0e4bcc0443c37fc0980a31354992
sergapit1 <serg.apit0@gmail.com>
2020-05-09 01:59:12 +0300

    2

--- src/by/academy/homework8/Main.java
+++ src/by/academy/homework8/Main.java
@@ -6,18 +6,19 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "Я люблю JAVA";
-        System.out.println(text.matches("h?"));
-        System.out.println(text.matches("h*"));
-        System.out.println(text.matches("h+"));
+        System.out.println(text.matches("h?"));      //
+        System.out.println(text.matches("h*"));      //  matches() метод пытается сопоставить всю строку с шаблоном
+        System.out.println(text.matches("h+"));      //
 
-        Pattern pattern1 = Pattern.compile("h?");
-        Pattern pattern2 = Pattern.compile("h*");
-        Pattern pattern3 = Pattern.compile("h+");
+
+        Pattern pattern1 = Pattern.compile("h?");   //  true: допустимо нулевое вхождение  X, once or not at all (X, один раз или ни разу)
+        Pattern pattern2 = Pattern.compile("h*");   //  true: допустимо нулевое вхождение  X, zero or more times (X, ноль или более раз)
+        Pattern pattern3 = Pattern.compile("h+");   //  false: недопустимо нулевое вхождение X, one or more times  (X, один или более раз)
 
         System.out.println("_________");
 
-        System.out.println(pattern1.matcher(text).find());
-        System.out.println(pattern2.matcher(text).find());
-        System.out.println(pattern3.matcher(text).find());
+        System.out.println(pattern1.matcher(text).find());   //
+        System.out.println(pattern2.matcher(text).find());   //  find() пытается найти вхождение шаблона в данной строке
+        System.out.println(pattern3.matcher(text).find());   //
     }
 }
